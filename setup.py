6f40d713a8a6a975b7a7004f5af692485c6b6ca5
WaldemarBjornstrom <84805366+WaldemarBjornstrom@users.noreply.github.com>
2021-05-26 10:17:08 +0200

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='SE_social',
     packages=find_packages(include=['sesocial']),
-    version='1.0.0',
+    version='1.1.0',
     description='A python library for verifying swedish social security numbers',
     author='WaldemarBjornstrom',
     license='MIT',
